92b59978d9106d054c5be033d9e0c44738081708
dougl <dougl@douglas>
2022-08-13 17:33:04 -0300

    Change-Id: I0000000000000000000000000000000000000000
    Change-Id: Ifc2035e3605757c5d822b9cbfa4294594a69b060

---
+++ primeiro- projeto-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,14 @@
+package cursojava.executavel;
+
+import java.nio.file.FileSystemNotFoundException;
+
+public class PrimeiraClasseJava {
+	/*É um metodo auto executavel no java*/
+	public static void main(String[] args) {
+		/*Escrever texto no console*/
+		System.out.println("Meu primeiro curso em java");
+		System.out.println("java é orientado a objeto");
+		
+	}
+
+}
