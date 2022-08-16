8676f7ee1b200edbafd00c4ccd6ac4ff4dd70f61
dougl <dougl@douglas>
2022-08-16 20:43:01 -0300

    data de nascimento

--- primeiro- projeto-java/src/cursojava/executavel/pessoa.java
+++ primeiro- projeto-java/src/cursojava/executavel/pessoa.java
@@ -1,5 +1,47 @@
 package cursojava.executavel;
 
 public class pessoa {
+	
+	private string name;
+	 
+	private string idade;
+	
+	private string datenascimento;
+	
+	public string getIdade() {
+		return idade;
+	}
+
+	public void setIdade(string idade) {
+		this.idade = idade;
+	}
+
+	public string getDatenascimento() {
+		return datenascimento;
+	}
+
+	public void setDatenascimento(string datenascimento) {
+		this.datenascimento = datenascimento;
+	}
+
+	private string sobrenome;
+
+	public string getName() {
+		return name;
+	}
+
+	public void setName(string name) {
+		this.name = name;
+	}
+
+	public string getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(string sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	
+	
 
 }
